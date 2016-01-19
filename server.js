5742e0f9af375415394c493bb3934d527cc6db4b
ziniulian <ziniulian@163.com>
2016-01-19 16:36:27 +0800

    添加静态文件的访问服务

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 #!/bin/env node
 // OpenShift sample Node application
 var express = require("express");
-var fs      = require("fs");
+
+// 引用文件模块
+var fs = require("fs");
+
+// 引用url模块
+var url = require("url");
+
+// 引用querystring模块
+var querystring = require("querystring");
+
+// 引用path模块
+var path = require("path");
 
 
 /**
@@ -94,13 +105,14 @@ var SampleApp = function() {
 	self.createRoutes = function() {
 		self.routes = { };
 
+		// 获取服务器的IP信息
 		self.routes["/ipconfig"] = function(req, res) {
 			var config = "IP地址： ";
 			config += self.ipaddress;
 			config += "<br>端口号： ";
 			config += self.port;
-			res.send("<html><head><title>服务器IP信息</title><meta charset='utf-8'/></head><body>" + config + "</body></html>");
-/*
+			// res.send("<html><head><title>服务器IP信息</title><meta charset='utf-8'/></head><body>" + config + "</body></html>");
+
 			res.writeHeader (200, {
 				"Access-Control-Allow-Origin": "*",		// HTML5 允许跨域访问的范围，* 代表允许任何网域访问
 				"Content-Type": "text/html; charset=utf-8",	// 文件格式； 字符编码
@@ -108,13 +120,77 @@ var SampleApp = function() {
 			});
 			res.write("<html><body>" + config + "</body></html>");
 			res.end();
-*/
 		};
 
+		// 获取静态文件
+		self.routes["/getStaticFile"] = function(request, response) {
+			// 解析参数
+			self.parseParam (request, response, function (request, response, param) {
+				// 返回静态文件
+				self.loadStaticFile (param.path, response);
+			});
+		};
+
+		// 访问主页
 		self.routes["/"] = function(req, res) {
 			res.setHeader("Content-Type", "text/html");
 			res.send(self.cache_get("index.html") );
 		};
+	};
+
+
+	// 解析 GET、POST 参数
+	self.parseParam = function(request, response, callback) {
+		var postData = "";
+
+		// 执行回调函数
+		var cb = function () {
+			callback (request, response, querystring.parse(postData));
+		};
+
+		if (request.method == "GET") {
+			postData = url.parse(request.url).query;
+			cb();
+		} else if (request.method == "POST") {
+			// 设置接收数据编码格式为 UTF-8
+			request.setEncoding("utf8");
+
+			// 因为nodejs在处理post数据的时候，会将数据分成小包来序列处理。所以必须监听每一个数据小包的结果
+			request.addListener("data", function (postDataChunk) {
+				postData += postDataChunk;
+			});
+	 
+			// 所有数据包接收完毕
+			request.addListener("end", cb);
+		}
+	};
+
+
+	// 返回静态文件
+	self.loadStaticFile = function (uri, response) {
+		var filename = path.join(process.cwd(), uri);
+		path.exists (filename, function (exists) {
+			if (!exists) {
+				response.writeHeader (404, {"Content-Type":"text/plain;charset=utf-8"});
+				response.write ("404 页面不存在\n", "utf-8");
+				response.end();
+			} else {
+				fs.readFile (filename, "binary", function (err, file) {
+					if (err) {
+						response.writeHeader (500, {"Content-Type":"text/plain;charset=utf-8"});
+						response.write((err + "\n"), "utf-8");
+						response.end();
+					} else {
+						response.writeHeader (200, {
+							"Access-Control-Allow-Origin": "*",		// HTML5 允许跨域访问的范围，* 代表允许任何网域访问
+							"Content-Type":"text/html; charset=utf-8"	// 文件格式； 字符编码
+						});
+						response.write(file, "binary");
+						response.end();
+					}
+				});
+			}
+		});
 	};
 
 
