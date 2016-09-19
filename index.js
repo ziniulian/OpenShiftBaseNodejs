20ec3982229efab31cf8d53351d5f6e62d53f6bd
ziniulian <ziniulian@163.com>
2016-09-19 17:33:19 +0800

    调试

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // LZR 模块加载
-console.log ("---0000---");
 require("LZR");
-console.log ("---0001---");
-console.log (process.env.OPENSHIFT_NODEJS_IP);
-console.log (process.env.OPENSHIFT_NODEJS_PORT);
-
+/*
 // LZR 子模块加载
 LZR.load([
 	"LZR.NodeJs.BaseMainSrv",
@@ -41,7 +37,7 @@ srv.start({
 	show: {
 		cls_: LZR.NodeJs.SampleWebFileSrv,
 		name: "/show/",
-		path: "/WorkSpace/work/",
+		path: "/WorkSpace/web/",
 		ajaxAllow: "*"
 	},
 
@@ -72,21 +68,25 @@ srv.start({
 		ajaxAllow: "*"
 	}
 });
+*/
 
-/*
+
 // LZR 子模块加载
 LZR.load([
 	"LZR.Node.Srv"
 ]);
-console.log ("---0002---");
 
 // 服务的实例化
 var srv = new LZR.Node.Srv ({
 	ip: process.env.OPENSHIFT_NODEJS_IP || "192.168.1.236",
 	port: process.env.OPENSHIFT_NODEJS_PORT || 8080
 });
-console.log ("---0003---");
 
+// srv.so.use(express.static(__dirname + '/public'));
+
+srv.so.listen(srv.port, srv.ip);
+
+/*
 // LZR库文件访问服务
 srv.ro.setStaticDir("/myLib/", LZR.curPath);
 
@@ -108,9 +108,7 @@ srv.use("*", function (req, res, next) {
 	res.writeHead(404, { "Content-Type": "text/plain" });
 	res.end("404!");
 });
-console.log ("---0004---");
 
 // 服务启动
 srv.start();
-console.log ("---0005---");
 */
