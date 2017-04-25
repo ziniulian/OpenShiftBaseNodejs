eedaa28da41cec1395e5d7eb89fd6f0fe3c39d47
ziniulian <ziniulian@163.com>
2017-04-25 16:31:54 +0800

    test

--- MyBlog/index.js
+++ MyBlog/index.js
@@ -9,6 +9,7 @@ eval("var curPath = " + LZR.getNodejsModelPath.toString() +
 // 数据库
 var mdb = new LZR.Node.Db ({
 	conf: process.env.OPENSHIFT_MONGODB_DB_URL ? process.env.OPENSHIFT_MONGODB_DB_URL : "mongodb://localhost:27017/test",
+	dbNam: process.env.OPENSHIFT_MONGODB_DB_URL ? "test" : "",
 	hd_sqls: {
 		srvGetBlog: {
 			tnam: "blog",
