9e06e2ec62b8ef2cde16833d9cec6eb1c7f811f6
ziniulian <ziniulian@163.com>
2017-04-25 16:35:35 +0800

    test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var srv = new LZR.Node.Srv ({
 // 数据库
 var mdb = new LZR.Node.Db ({
 	conf: process.env.OPENSHIFT_MONGODB_DB_URL ? process.env.OPENSHIFT_MONGODB_DB_URL : "mongodb://localhost:27017/test",
-	dbNam: process.env.OPENSHIFT_MONGODB_DB_URL ? "test" : "",
+	dbNam: "test",
 	hd_sqls: {
 		srvTrace: {
 			tnam: "vs",
