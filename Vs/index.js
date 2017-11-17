ba2c54e84a6a8bc137b0c297b5ac2e614cf718ee
ziniulian <ziniulian@163.com>
2017-11-17 16:06:52 +0800

    o3测试

--- Vs/index.js
+++ Vs/index.js
@@ -10,37 +10,38 @@ LZR.load([
 
 var utNode = LZR.getSingleton(LZR.Node.Util);
 
-var getMongoURL = function () {
-	var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
-
-	if (!mongoURL && process.env.DATABASE_SERVICE_NAME) {
-		var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-		mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-		mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-		mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-		mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-		mongoUser = process.env[mongoServiceName + '_USER'];
-
-		if (mongoHost && mongoPort && mongoDatabase) {
-			mongoURL = 'mongodb://';
-			if (mongoUser && mongoPassword) {
-				mongoURL += mongoUser + ':' + mongoPassword + '@';
-			}
-			// Provide UI label that excludes user id and pw
-			mongoURL += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-		}
-	}
-
-	if (mongoURL) {
-		return mongoURL;
-	} else {
-		return "mongodb://localhost:27017/test";
-	}
-}
+// var getMongoURL = function () {
+// 	var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
+//
+// 	if (!mongoURL && process.env.DATABASE_SERVICE_NAME) {
+// 		var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+// 		mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+// 		mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+// 		mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+// 		mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+// 		mongoUser = process.env[mongoServiceName + '_USER'];
+//
+// 		if (mongoHost && mongoPort && mongoDatabase) {
+// 			mongoURL = 'mongodb://';
+// 			if (mongoUser && mongoPassword) {
+// 				mongoURL += mongoUser + ':' + mongoPassword + '@';
+// 			}
+// 			// Provide UI label that excludes user id and pw
+// 			mongoURL += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+// 		}
+// 	}
+//
+// 	if (mongoURL) {
+// 		return mongoURL;
+// 	} else {
+// 		return "mongodb://localhost:27017/test";
+// 	}
+// }
 
 // 数据库
 var mdb = new LZR.Node.Db.Mongo ({
-	conf: getMongoURL(),
+	// conf: getMongoURL(),
+	conf: process.env.OPENSHIFT_MONGODB_DB_URL || "mongodb://localhost:27017/test",
 	autoErr: true,
 	hd_sqls: {
 		srvTrace: {
--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var ajax = new LZR.Node.Db.NodeAjax ({
 // 服务的实例化
 var srv = new LZR.Node.Srv ({
 	ip: process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
-	port: process.env.OPENSHIFT_NODEJS_PORT || 8080
+	port: process.env.OPENSHIFT_NODEJS_PORT || 80
 });
 
 // LZR库文件访问服务
